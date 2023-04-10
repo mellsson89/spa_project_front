d6b1da3734f7a012be2faf76c4fbb167809fc284
Serg <serg@MacBook-Pro-Serg.local>
2023-04-10 14:44:45 +0300

    update api-service.js

--- src/service/api-service.js
+++ src/service/api-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL='https://backend-telegram-kappa.vercel.app/'
+axios.defaults.baseURL='https://spa-project-backend.vercel.app'
 
 export const getAllComments = async (page=1, limit=25, key = 'createdAt', sort='DESC') => {
     try {
