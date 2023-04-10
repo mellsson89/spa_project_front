1e2922d1891fb8c012a45db94744ca94a3651f7f
Serg <serg@MacBook-Pro-Serg.local>
2023-04-10 15:13:51 +0300

    update api-service.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,8 +1,3 @@
 import {io} from "socket.io-client";
 
-export const socket = io("https://spa-project-backend.vercel.app",{
-    withCredentials: true,
-    extraHeaders: {
-        "my-custom-header": "abcd"
-    }
-});
+export const socket = io("https://spa-project-backend.vercel.app");
