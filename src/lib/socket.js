ab8b10ad1795d17fd765c2f3a4f0f396a982d308
Serg <serg@MacBook-Pro-Serg.local>
2023-04-11 18:49:02 +0300

    update api-service.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 
 
-export const socket = io("https://spa-project-backend.vercel.app");
+export const socket = io("https://spa-backend.pp.ua");
 
 console.log(socket)
