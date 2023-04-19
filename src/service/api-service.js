e412f94efdaf3668681207318869211b53209fad
Serg <serg@MacBook-Pro-Serg.local>
2023-04-19 15:47:27 +0300

    update project

--- src/service/api-service.js
+++ src/service/api-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL='http://localhost:5000'
+axios.defaults.baseURL='https://spa-backend.pp.ua'
 
 export const getAllComments = async (page=1, limit=25, key = 'createdAt', sort='DESC') => {
     try {
